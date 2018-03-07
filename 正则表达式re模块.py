babbbe8b83072e5c9857b92419fdfe356c0c3aa0
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2018-03-07 21:37:53 +0800

    Update 正则表达式re模块.py

--- 正则表达式re模块.py
+++ 正则表达式re模块.py
@@ -6,15 +6,28 @@
 
 
 
+import re  #导入python正则表达式模块
+pa = re.compile(r'imooc\n')  #pa = re.compile('imooc\\n')  -->生成pattern的实例
+print(pa)  #re.compile('imooc\\n')
+print(type(pa))  #<class '_sre.SRE_Pattern'>
 
+pa = re.compile(r'imooc')
+print(pa)  #re.compile('imooc')
+print(type(pa))  #<class '_sre.SRE_Pattern'>
 
+str1 = 'imooc python'
+print(pa.match(str1))  #<_sre.SRE_Match object; span=(0, 5), match='imooc'>
 
+ma = pa.match(str1)
+print(ma.group())  #imooc  -->group()返回str或者tuple
+print(ma.span())  #(0, 5)  -->匹配的位置
 
+print(ma.string)  #imooc python  -->被匹配的字符串会放在string中
+print(ma.re)  #re.compile('imooc')  -->Pattern实例会放在re中
 
-
-
-
-
+pa1 = re.compile(r'_')
+ma1 = pa1.match('_value')
+print(ma1.group())  #_
 
 
 
