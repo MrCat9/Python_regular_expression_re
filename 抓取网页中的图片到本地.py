42134fdcf66ec3ada5011fa5270c2df8888ce5ab
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2018-03-10 21:05:58 +0800

    Create 抓取网页中的图片到本地.py

---
+++ 抓取网页中的图片到本地.py
@@ -0,0 +1,31 @@
+#步骤：
+#1.抓取网页
+#2.获取图片地址
+#3.抓取图片内容并保存到本地
+
+from urllib.request import urlopen
+import re
+
+req = urlopen('https://www.imooc.com/')
+buf = req.read()
+print(buf)
+
+buf = buf.decode('utf-8')
+# listurl = re.findall(r'src=.+\.jpg', buf)
+# print(listurl)
+# listurl = re.findall(r'http:.+\.jpg', buf)
+# print(listurl)
+listurl = re.findall(r'\//img.+\.jpg', buf)
+print(listurl)
+
+i = 0
+for url in listurl:
+    if url is None:
+        continue
+    f = open('img'+str(i)+'.jpg', 'wb')  #'wb'-->使用二进制写方式打开文件
+    fullurl = 'https:'+url
+    req = urlopen(fullurl)
+    buf = req.read()
+    f.write(buf)
+    f.close()
+    i+=1
